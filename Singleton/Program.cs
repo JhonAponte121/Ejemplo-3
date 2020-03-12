df4b229a272b2638f3715f06b670db42cba50a4f
JhonAponte121 <jhonaponte121@hotmail.com>
2020-03-11 21:00:18 -0400

    version1

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+
+        protected Singleton()
+        {
+            mensaje = "Durante este proceso se a utilizado el patron de diseño Singleton";
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+
+    }
+}
